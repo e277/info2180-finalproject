ea7fbe36e7fd213d44fe057122f972507aef89aa
e277 <ezramuir12@gmail.com>
2023-11-23 21:49:09 -0500

    Write viewUser boilerplate

---
+++ viewUser.php
@@ -0,0 +1,31 @@
+<?php
+require "header.php";
+require "sidebar.php";
+?>
+
+<section>
+    <div>
+        <h1>Users</h1>
+        <button>+ Add User</button>
+    </div>
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+            <th>Created</th>
+        </tr>
+        <tr>
+            <td>John Doe</td>
+            <td>johndoe@gmail.com</td>
+            <td>Member</td>
+            <td>2020-01-01</td>
+    </table>
+    
+</section>
+
+
+<?php
+// php processing
+
+?>
